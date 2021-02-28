41bfce5dae08492ae91d57b90f3db719ef0a37c4
范轶洁 <fanyjb@digitalchina.com>
2021-02-28 15:37:28 +0800

    step 1

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import "./index.css";
 
 const Page = () => {
-  return <div className="home">HOME</div>;
+  return (
+    <div className="home">
+      <h2>
+        <span>H</span>OME / <span>Lo</span>rem ipsum dolor sit amet.
+      </h2>
+    </div>
+  );
 };
 
 export default Page;
