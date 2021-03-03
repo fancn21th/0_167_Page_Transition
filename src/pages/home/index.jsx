befbc4f2982213605d5e074461f39862b37c37f7
范轶洁 <fanyjb@digitalchina.com>
2021-03-03 22:22:09 +0800

    tickets refactor as component

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,51 +1,13 @@
-import React, { useEffect, useRef, useState } from "react";
-import hoverEffect from "hover-effect";
+import React from "react";
 import Grid from "./layout";
+import Ticket from "./components/ticket";
 import "./index.css";
 
 const Page = () => {
-  const he = useRef(null);
-  const [toggle, setToggle] = useState(false);
-
-  useEffect(() => {
-    he.current = new hoverEffect({
-      parent: document.querySelector(".ticket"),
-      intensity1: 0.1,
-      intensity2: 0.1,
-      angle2: Math.PI / 2,
-      hover: false,
-      image1:
-        "https://images.unsplash.com/photo-1518824547657-2bb07e2978ba?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&h=900&fit=crop&ixid=eyJhcHBfaWQiOjF9&s=942862481ea16bf420e9014eb479e866",
-      image2:
-        "https://images.unsplash.com/photo-1494756739853-38af58aa9dd0?ixlib=rb-0.3.5&s=121d4a13a0ca8125ea7bb70eae292900&auto=format&fit=crop&w=1351&q=80",
-      displacementImage:
-        "https://cdn.rawgit.com/robin-dela/hover-effect/b6c6fd26/images/stripe1mul.png?raw=true",
-    });
-  }, []);
-
-  const onClick = () => {
-    if (!toggle) he.current.next();
-    else he.current.previous();
-    setToggle(!toggle);
-  };
-
   return (
     <>
       <Grid />
-      <div className="ticket" onClick={onClick}>
-        <div className="overlay"></div>
-        <div className="flight-info">
-          <h3>JUNE 30 2018 12:30PM</h3>
-          <div className="flight-locations">
-            <h1>LAX</h1>
-            <img
-              alt="flight"
-              src="https://cdn-images-1.medium.com/max/800/1*QFU_XYAKOGJ9nunfqvem1w.png"
-            />
-            <h1>JFK</h1>
-          </div>
-        </div>
-      </div>
+      <Ticket />
     </>
   );
 };
