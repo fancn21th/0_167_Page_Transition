571054e610a0f9b691414dc39fe64a29c6abbdff
范轶洁 <fanyjb@digitalchina.com>
2021-03-03 16:05:54 +0800

    forwards ref

--- src/components/expander/index.jsx
+++ src/components/expander/index.jsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { gsap } from "gsap";
 import LEFT from "./components/expander-item-left";
 import MID from "./components/expander-item-mid";
 import RIGHT from "./components/expander-item-right";
 
 const Expander = () => {
+  const left = React.createRef();
+  const mid = React.createRef();
+  const right = React.createRef();
+
+  useEffect(() => {});
+
   return (
     <>
-      <LEFT />
-      <MID />
-      <RIGHT />
+      <LEFT ref={left} />
+      <MID ref={mid} />
+      <RIGHT ref={right} />
     </>
   );
 };
