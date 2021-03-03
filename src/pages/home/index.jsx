4f97829e9a4939fe2c5428af9f5130612af00c4a
范轶洁 <fanyjb@digitalchina.com>
2021-03-03 22:37:08 +0800

    basic integration

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Grid from "./layout";
 import Ticket from "./components/ticket";
+import Map from "./components/map";
 import "./index.css";
 
 const Page = () => {
   return (
     <>
       <Grid />
-      <Ticket />
+      <Map />
+      {/* <Ticket /> */}
     </>
   );
 };
