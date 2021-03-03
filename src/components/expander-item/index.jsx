d0971fbca48b4404897dd367227cfe73c670d217
范轶洁 <fanyjb@digitalchina.com>
2021-03-03 15:05:01 +0800

    expander-item

--- src/components/expander-item/index.jsx
+++ src/components/expander-item/index.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
+import { gsap } from "gsap";
 import "./index.css";
 
 const ExpanderItem = ({ width = "400px", height = "400px" }) => {
+  const item = useRef();
+  const image = useRef();
+  const tl = gsap.timeline({
+    paused: true,
+  });
+
+  useEffect(() => {
+    tl.to(
+      [item.current],
+      1.5,
+      {
+        width: 1200,
+      },
+      "first"
+    );
+    tl.to(
+      [image.current],
+      1.5,
+      {
+        x: 0,
+      },
+      "first"
+    );
+  }, []);
+
+  const expand = () => {
+    tl.play();
+  };
+
+  const shrink = () => {
+    tl.reverse();
+  };
+
   return (
     <div
       className="expander-item"
@@ -9,8 +43,11 @@ const ExpanderItem = ({ width = "400px", height = "400px" }) => {
         width,
         height,
       }}
+      ref={item}
+      onMouseOver={expand}
+      onMouseLeave={shrink}
     >
-      <div className="expander-item__image-container"></div>
+      <div className="expander-item__image-container" ref={image}></div>
     </div>
   );
 };
