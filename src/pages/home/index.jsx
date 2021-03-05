8c450c102bafb42a09687a3873f825b39730fbab
范轶洁 <fanyjb@digitalchina.com>
2021-03-05 11:14:10 +0800

    fored globe integration

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import Grid from "./layout";
-import Ticket from "./components/ticket";
-import Map from "./components/map";
+// import Ticket from "./components/ticket";
+// import Map from "./components/map";
+import Globe from "./components/globe";
 import "./index.css";
 
 const Page = () => {
   return (
     <>
       <Grid />
-      <Map />
+      <Globe />
+      {/* <Map /> */}
       {/* <Ticket /> */}
     </>
   );
